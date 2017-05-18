17748e53f81f79e5aafa3a45899ff0b25353fd34
jazzysnail <jazzysnail@outlook.com>
2017-05-18 23:37:52 +0800

    文章页组件细化

--- bisheng-theme-biu/template/post.jsx
+++ bisheng-theme-biu/template/post.jsx
@@ -1,45 +1,79 @@
 import React from 'react';
-
 // style
 import '../static/style';
-
 // components
 import Layout from './layout';
 
 export default function (props) {
-
   const postName = props.router.params.post;
   const { toReactComponent } = props.utils;
   const pageData = props.pageData;
-
-  let Content = function() {
+  // 日期
+  const Date = function(props) {
+    if (props.date) {
+      return (
+        <span>
+          <i className="mineicon mineicon-clock"></i>
+          <time>{props.date.slice(0, 10)}</time>
+        </span>
+      )
+    }else{
+      return null
+    }
+  }
+  // 标签
+  const Tags = function(props) {
+    if (props.tags) {
+      return (
+        <span>
+          <i className="mineicon mineicon-sale"></i>
+          <span className="tags">
+            {
+              props.tags.map((val,index) => {
+                return <a href="/" key={val + index}>{val}</a>
+              })
+            }
+          </span>
+        </span>
+      )
+    }else{
+      return null
+    }
+  }
+  // 内容
+  const Content = function(props) {
     return React.createElement(
-      'div',      { className: 'entry-content' },
+      'div',
+      { className: 'entry-content' },
       toReactComponent(pageData.content)
     )
   }
+  // 分页
+  const Pager = function(props) {
+    return (
+      <div className="post-footer">
+        <a className="previous">
+          <i className="mineicon mineicon-arrowleft"></i>
+          &nbsp;prev
+        </a>
+        <span>⌘+e back to news  ⌘+enter post comment</span>
 
-  let Tags = pageData.meta.tags.map((val,index) => {
-    return <a href="/" key={val + index}>{val}</a>
-  });
+        <a className="next">next&nbsp;
+          <i className="mineicon mineicon-arrowright"></i>
+        </a>
+      </div>
+    )
+  }
 
   return (
     <Layout themeConfig={props.themeConfig}>
-
       <div className="post-info">
-        <i className="mineicon mineicon-clock"></i>
-        <time>{pageData.meta.date.slice(0, 10)}</time>
-        <i className="mineicon mineicon-sale"></i>
-        <span className="tags">{Tags}</span>
+        <Date date={pageData.meta.date}/>
+        <Tags tags={pageData.meta.tags} />
       </div>
-
       <h1>{pageData.meta.title}</h1>
       <Content />
-      <div className="post-footer">
-        <a className="previous">← previous post</a>
-        <span>⌘+e back to news  ⌘+enter post comment</span>
-        <a className="next">next post →</a>
-      </div>
+      <Pager />
     </Layout>
   )
 }
---
+++ posts/hello-blog.md
@@ -0,0 +1,12 @@
+---
+title: Hello World
+date: 2016-07-13 21:48:04
+---
+
+依照惯例还是先来一个"hello world"。使用hexo建立了自己的blog，也许就开始写博文了罢，或许是笔记？心得？灵感？总之就是慢慢写点东西出来。
+
+---
+
+博客目前部署在github上面，下面是地址，欢迎来看看。
+-  github: [jazzysnail](https://github.com/jazzysnail)
+-  blog: [leon's blog](http://jazzysnail.github.io/)
